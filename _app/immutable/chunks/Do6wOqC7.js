1c2fde654d2d2355cee303f81096dfbb0d4cf96d
Alair Dias Junior <alair@alair.me>
2026-02-15 18:18:30 -0500

    Add support to logs

---
+++ _app/immutable/chunks/Do6wOqC7.js
@@ -0,0 +1,1 @@
+import{b as s,h as c,a as m,E as h}from"./BiW7Hae7.js";import{B as i}from"./7YaJw6p3.js";function E(n,r,o){c&&m();var e=new i(n);s(()=>{var a=r()??null;e.ensure(a,a&&(t=>o(t,a)))},h)}export{E as c};
