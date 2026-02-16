073f8258c2ba8a825b2c0f49c2345ec84bc634be
Alair Dias Junior <alair@alair.me>
2026-02-16 00:45:13 -0500

    Improve logs and remove unused files

--- _app/immutable/entry/start.BYVVD-kD.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/B8c4b1FJ.js";export{o as load_css,r as start};
--- _app/immutable/entry/start.CpASAM8k.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Dzb644AK.js";export{o as load_css,r as start};
